b7a12ea872ea4abc327a4ff560dbd1d14757e868
Si7H <arkadiusz.mecka@gmail.com>
2021-10-31 16:52:56 +0100

    add get/userId functionality

--- src/main/java/com/deadlifter/service/impl/UserServiceImpl.java
+++ src/main/java/com/deadlifter/service/impl/UserServiceImpl.java
@@ -63,4 +63,17 @@ public class UserServiceImpl implements UserService {
         if(userEntity == null) throw new UsernameNotFoundException(email);
         return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+
+        UserDto returnValue = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(userId);
+
+        if(userEntity == null)
+            throw new UsernameNotFoundException(userId);
+
+        BeanUtils.copyProperties(userEntity, returnValue);
+        return returnValue;
+    }
 }
